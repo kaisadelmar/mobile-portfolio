9e55d4c8f0ec4f64f9ac493501e55ea3cb22635a
Kaisa Martiskainen <eevakaisab@gmail.com>
2016-01-22 16:07:50 -0500

    Optimize loop for updatePositions

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -501,13 +501,16 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
-function updatePositions() {
+// Optimized another for loop to reduce multiple calculations
+  function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
+  var scrolling = document.body.scrollTop / 1250;
+  var phase;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrolling + i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
